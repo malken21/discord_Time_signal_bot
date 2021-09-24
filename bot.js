d6ea819b5d5b7a3f30bd4c2becfbba027adfcd3b
malken21 <77374813+malken21@users.noreply.github.com>
2021-09-24 17:47:29 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,13 @@
 const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 const cron = require('node-cron');
-
 const Config = require("./Config.json")
 client.login(Config.TOKEN);
-
 cron.schedule(Config.time, () => {
-  const message_list = Config.messages;
-  client.guilds.cache.get(Config.server_name).channels.cache.find(ch => ch.name === Config.channel_name).send(message_list[Math.floor( Math.random()*message_list.length)])
+  const message = Config.messages[Math.floor( Math.random()*Config.messages.length)];
+  client.guilds.cache.get(Config.server_id).channels.cache.get(Config.channel_id).send(message)
+  console.log(`\nServerID : ${Config.server_id}\nchannelID : ${Config.channel_id}\nMessage : ${message}`);
 })
-
 client.on('ready', () => {
   const time = new Date()
   console.log(`${time}\nlogin!!(${client.user.tag})`);
