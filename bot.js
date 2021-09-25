a0a403d86d0dcd25742fbf7549f53abe36cddff5
malken21 <77374813+malken21@users.noreply.github.com>
2021-09-25 16:33:05 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ const cron = require('node-cron');
 const Config = require("./Config.json")
 client.login(Config.TOKEN);
 cron.schedule(Config.time, () => {
-  const message = Config.messages[Math.floor( Math.random()*Config.messages.length)];
-  client.guilds.cache.get(Config.server_id).channels.cache.get(Config.channel_id).send(message)
-  console.log(`\nServerID : ${Config.server_id}\nchannelID : ${Config.channel_id}\nMessage : ${message}`);
+  var random = Math.floor(Math.random()*Config.messages.length);
+  client.guilds.cache.get(Config.server_id).channels.cache.get(Config.channel_id).send(Config.messages[random])
+  console.log(`\nServerID : ${Config.server_id}\nchannelID : ${Config.channel_id}\nRandom : ${random}\nMessage : ${message}`);
 })
 client.on('ready', () => {
   const time = new Date()
